2d963078954a20e0856f6a89ef8d7812af15f302
PTB <Binh.pt2413992@sis.hust.edu.vn>
2025-10-09 20:59:41 +0700

    Add project files.

---
+++ dhdfuf/dhdfuf.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int c;
+	scanf_s("%d", &c);	
+	switch (c) {
+	    case 1:printf("Cau truc du lieu & giai thuat"); break;
+		case 2:printf("Lap trinh C & C++"); break;
+		case 3:printf("Ma lop 163082"); break;
+		case 4:printf("Chuc mung nam moi"); break;
+		default:printf("Khong co gi de in"); break;
+	}
+	return 0;
+}

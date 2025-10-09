93871acfbaec0b7c48208862c7da0b89683367c6
PTB <Binh.pt2413992@sis.hust.edu.vn>
2025-10-09 22:07:04 +0700

    bai tap git

--- dhdfuf/dhdfuf.cpp
+++ dhdfuf/dhdfuf.cpp
@@ -6,7 +6,7 @@ int main() {
 	    case 1:printf("Cau truc du lieu & giai thuat"); break;
 		case 2:printf("Lap trinh C & C++"); break;
 		case 3:printf("Ma lop 163082"); break;
-		case 4:printf("Chuc mung nam moi"); break;
+		case 4:printf("Chuc mung nam moi"); break;		
 		default:printf("Khong co gi de in"); break;
 	}
 	return 0;
